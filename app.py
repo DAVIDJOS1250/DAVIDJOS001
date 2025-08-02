45894e195d908e3e8a250b6f79898a3b903a2798
DAVIDJOS1250 <oluwayimikadavid419@gmail.com>
2025-08-02 23:58:30 +0100

    Update app.py
    First file

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-  
+  uvicorn app:app --host 0.0.0.0 --port $PORT
